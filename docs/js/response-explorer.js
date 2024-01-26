b1aa05c86a637ef5587a43fcc58dd61a00e444be
Kevin <kjfeng@uw.edu>
2024-01-25 22:31:49 -0800

    new data + code cleanup

--- docs/js/response-explorer.js
+++ docs/js/response-explorer.js
@@ -1,165 +1,7 @@
 'use strict';
 
-function loadHTMLChart(id, caseName) {
-  let htmlFilepath = "https://social.cs.washington.edu/case-law-ai-policy/assets/charts/" + caseName + ".html";
-  $(id).load(htmlFilepath);
-}
-
-function boldNameAndAffiliation(id) {
-  document.getElementById(id).style.fontWeight = 700;
-  if (id === "king") {
-    document.getElementsByClassName("independent")[0].style.fontWeight = 700;
-  } else {
-    document.getElementsByClassName("uw")[0].style.fontWeight = 700;
-  }
-}
-
-function unboldNameAndAffiliation(id) {
-  document.getElementById(id).style.fontWeight = 400;
-  if (id === "king") {
-    document.getElementsByClassName("independent")[0].style.fontWeight = 400;
-  } else {
-    document.getElementsByClassName("uw")[0].style.fontWeight = 400;
-  }
-}
-
-function fetchDimensions(input_id) {
-  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/cases.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then (function (data) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === input_id) {
-        let target = document.getElementById('dimensions-display');
-        let dimensions = data[i].dimensions;
-        let innerHTML = "";
-        for (let j = 0; j < dimensions.length; j++) {
-          let dimension = dimensions[j];
-          let cardHTML = `<div class="card dimension-card" style="width: 18rem;"><div class="card-header">${dimension.description}</div><ul class="list-group list-group-flush"><li class="list-group-item"><span class="quote-text">${dimension.quote}</span></li></ul></div>`;
-          innerHTML += cardHTML;
-        }
-        target.innerHTML = innerHTML;
-        document.getElementById('dimensions-placeholder').style.display = "none";
-      }
-    }
-  })
-}
-
-function revealDimensionSelector(input_id) {
-  document.getElementById('generated-case').style.display = "none";
-  document.getElementById('level-selector').style.display = "none";
-
-  sessionStorage.setItem('selectedCaseID', input_id);
-  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/cases.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then (function (data) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === input_id) {
-        let target = document.getElementById('dimension-selector');
-        let dimensions = data[i].dimensions;
-        let innerHTML = "<option selected>Select dimension</option>";
-        for (let j = 0; j < dimensions.length; j++) {
-          let dimension = dimensions[j];
-          let formOptionHTML = `<option value=${j+1}>${dimension.description}</option>`;
-          innerHTML += formOptionHTML;
-        }
-        target.innerHTML = innerHTML;
-        document.getElementById('dimension-selector').style.display = "block";
-      }
-    }
-  })
-}
-
-function revealLevelSelector() {
-  document.getElementById('dimension-selector').addEventListener('change', function () {
-    document.getElementById('generated-case').style.display = "none";
-    let dimensionSelector = document.getElementById('dimension-selector');
-    let selectedDimensionIndex = dimensionSelector.value;
-    if (dimensionSelector.options[selectedDimensionIndex]) {
-      sessionStorage.setItem('selectedDimension', dimensionSelector.options[selectedDimensionIndex].text);
-      let target = document.getElementById('level-selector');
-
-      return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/cases.json')
-      .then(function (response) {
-        return response.json();
-      })
-      .then (function (data) {
-        let selectedCaseID = sessionStorage.getItem('selectedCaseID');
-        let generatedAssets = data.find(x => x.id === selectedCaseID).dimensions[selectedDimensionIndex-1].generated
-        let innerHTML = "<option selected>Select level</option>";
-        for (let j = 0; j < generatedAssets.length; j++) {
-          let level = generatedAssets[j].level;
-          let formOptionHTML = `<option value=${j+1}>${level}</option>`;
-          innerHTML += formOptionHTML;
-        }
-        target.innerHTML = innerHTML;
-        document.getElementById('level-selector').style.display = "block";
-      })
-
-    } else {
-      document.getElementById('level-selector').style.display = "none";
-    }
-
-  })
-}
-
-function generatedCaseSelector() {
-  document.getElementById('level-selector').addEventListener('change', function () {
-    let levelSelector = document.getElementById('level-selector');
-    let selectedLevelIndex = levelSelector.value;
-    let target = document.getElementById('generated-case');
-    let targetWrapper = document.getElementsByClassName('chat-ui-wrapper')[1];
-    if (levelSelector.options[selectedLevelIndex]) {
-      target.innerHTML = "";
-
-      return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/cases.json')
-      .then(function (response) {
-        return response.json();
-      })
-      .then (function (data) {
-        let selectedCaseID = sessionStorage.getItem('selectedCaseID');
-        let findCase = data.find(x => x.id === selectedCaseID)
-        let selectedDimension = sessionStorage.getItem('selectedDimension');
-        let findDimension = findCase.dimensions.find(x => x.description === selectedDimension);
-        let generatedCase = findDimension.generated[selectedLevelIndex-1].response;
-        target.style.display = "block";
-        targetWrapper.style.display = "block";
-
-        let i = 0;
-        function typewriterEffect() {
-          if (i < generatedCase.length) {
-            target.innerHTML += generatedCase.charAt(i);
-            i++;
-            setTimeout(typewriterEffect, 0.1);
-          }
-        }
-
-        setTimeout(() => { typewriterEffect(); }, 500);
-
-        // target.innerHTML = generatedCase;
-      })
-
-    } else {
-      target.style.display = "none";
-      targetWrapper.style.display = "none";
-    }
-
-
-  })
-}
-
 function getResponseFromTemplate(templateID) {
-  let caseSelector = document.getElementById('case-selector');
-  // let selectedValue = caseSelector.value;
-  // caseSelector.addEventListener('change', function () {
-  //   sessionStorage.setItem('caseValueIndex', selectedValue);
-  // });
-
-
-  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data-example.json')
+  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data.json')
     .then(function (response) {
       return response.json();
     })
@@ -180,8 +22,9 @@ function getResponseFromTemplate(templateID) {
       if (templateID === "content-violation") {
         target.innerHTML = response;
       } else {
+        response = marked.parse(response);
         let i = 0, isTag, text;
-        // target.innerHTML = response;
+
         function typewriterEffectFormatted() {
           let latestStartTime = parseInt(sessionStorage.getItem('startTimeStamp'));
           if (latestStartTime !== startTimeStamp) return;
@@ -217,7 +60,7 @@ function revealCaseAndTemplates() {
     let caseValueIndex = caseSelector.value;
     sessionStorage.setItem('caseValueIndex', caseValueIndex);
 
-    return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data-example.json')
+    return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data.json')
     .then(function (response) {
       return response.json();
     })
@@ -236,7 +79,7 @@ function revealCaseAndTemplates() {
 function loadCaseOptions() {
   let target = document.getElementById('case-selector');
 
-  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data-example.json')
+  return fetch('https://social.cs.washington.edu/case-law-ai-policy/assets/response-explorer-data.json')
   .then(function (response) {
     return response.json();
   })
@@ -251,36 +94,10 @@ function loadCaseOptions() {
   })
 }
 
-const resizeObserver = new ResizeObserver((entries) => {
-  for (const entry of entries) {
-    let elementID = entry.target.id;
-    const { width, height } = entry.contentRect;
-    if (elementID === "case-select" || elementID === "case-select-p2") {
-      let element = document.getElementById(elementID);
-      if (width > 1200) {
-        element.style.justifyContent = "center";
-      } else {
-        element.style.justifyContent = "flex-start";
-      }
-    }
-    if (elementID === "dimensions-display") {
-      if (width > 1000) {
-        document.getElementById('dimensions-display').style.justifyContent = "center";
-      } else {
-        document.getElementById('dimensions-display').style.justifyContent = "flex-start";
-      }
-    }
-  }
-})
-
 
 window.onload = function () {
   loadCaseOptions();
   revealCaseAndTemplates();
-
-  resizeObserver.observe(document.getElementById('case-select'));
-  resizeObserver.observe(document.getElementById('case-select-p2'));
-  resizeObserver.observe(document.getElementById('dimensions-display'));
   sessionStorage.clear();
 }
 
